c63ecfc0cde84eba7b44823199b252e653331e2d
sipuli93 <66952910+sipuli93@users.noreply.github.com>
2020-11-12 10:21:27 +0200

    Update MMM-IndoorAndSaunaTemp.js

--- MMM-IndoorAndSaunaTemp.js
+++ MMM-IndoorAndSaunaTemp.js
@@ -3,24 +3,44 @@
 /* Magic Mirror
  * Module: MMM-IndoorAndSaunaTemp
  *
- * By 
+ * By sipuli93
  * MIT Licensed.
  */
 
 Module.register("MMM-IndoorAndSaunaTemp", {
-	defaults: {	
+	defaults: {
+		saunaTempLimit: 35
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
 	start: function() {
 		var self = this;
+		this.indoorSensor = {
+			MAC: this.config.indoorSensorMAC,
+			temp: NaN
+		};
+		this.saunaSensor = {
+			MAC: this.config.saunaSensorMAC,
+			temp: NaN
+		};
 	},
 	
 	// Generate dom object for module
 	getDom: function() {
 		var self = this;
 		var wrapper = document.createElement("div");
+		var degreeLabel = "°";
+		if (this.indoorSensor.temp != NaN) {
+			var indoorTemp = document.createElement("H1");
+			indoorTemp.innerHTML = this.indoorSensor.temp + degreeLabel + " C";
+			wrapper.appendChild(indoorTemp);
+		}
+		if (this.saunaSensor.temp != NaN && this.saunaSensor.temp > this.config.saunaTempLimit) {
+			var saunaTemp = document.createElement("H1");
+			saunaTemp.innerHTML = this.saunaSensor.temp + degreeLabel + " C";
+			wrapper.appendChild(saunaTemp);
+		}
 		return wrapper;
 	},
 	
@@ -37,5 +57,23 @@ Module.register("MMM-IndoorAndSaunaTemp", {
 			en: "translations/en.json",
 			es: "translations/es.json"
 		};
+	},
+	
+	// Override socket notification handler.
+	notificationReceived: function (notification, payload, sender) {
+		var self = this;
+		
+		if (notification == 'RUUVI_ENVIRONMENT_PACKET') {
+			var obj = JSON.parse(payload);
+			
+			if (this.indoorSensor.MAC === obj.MAC) {
+				this.indoorSensorTemp = obj.temperature;
+				self.updateDom();
+			}
+			else if (this.saunaSensor.MAC === obj.MAC) {
+				this.saunaSensorTemp = obj.temperature;
+				self.updateDom();
+			}
+		}
 	},
 });
